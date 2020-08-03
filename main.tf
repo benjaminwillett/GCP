30213e40e8a1127e50927ad58ca091687823c2d9
Ben Willett <ben.willett@distortenterprises.com>
2020-08-03 13:20:56 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "google" {
   version = "3.5.0"
 
-  credentials = file(var.credentials_file)
+  credentials = file(var.credentials)
 
   project = "terraform-project1-285302"
   region  = "australia-southeast1"
