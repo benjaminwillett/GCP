5807d6963cfacfbbcf38d4bd960cb326186b66d8
Ben Willett <ben.willett@distortenterprises.com>
2020-08-04 10:42:00 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    network = google_compute_network.vpc_network.vpc_network
+    network = google_compute_network.vpc_network.name
     access_config {
     }
   }
